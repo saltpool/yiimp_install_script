8f63999b039e5be53c2767efae657b9a364150e8
eskal <65866311+eskal@users.noreply.github.com>
2020-07-16 01:44:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -177,10 +177,9 @@
     #hide_output sudo phpenmod mcrypt
     #hide_output sudo phpenmod mbstring
     else
-    apt_install php7.3-fpm php7.3-opcache php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-imap php7.3-cli \
-    php7.3-cgi php-pear imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt\
-    php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache php-imagick php-gettext php7.3-zip php7.3-mbstring \
-    libpsl-dev libnghttp2-dev
+    apt_install php7.2-fpm php7.2-opcache php7.2 php7.2-common php7.2-gd php7.2-mysql php7.2-imap php7.2-cli \
+    php7.2-cgi php-pear php-auth imagemagick libruby php7.2-curl php7.2-intl php7.2-pspell mcrypt\
+    php7.2-recode php7.2-sqlite3 php7.2-tidy php7.2-xmlrpc php7.2-xsl memcached php-memcache php-imagick php-gettext php7.2-zip php7.2-mbstring
     fi
     sleep 5
     hide_output sudo systemctl start php7.2-fpm
